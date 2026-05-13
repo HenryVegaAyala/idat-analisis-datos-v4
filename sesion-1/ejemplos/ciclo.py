9cf2476d318f5978952b16a240b91284f73d8c23
Henry Vega <hp.vega@hotmail.com>
2026-05-12 22:04:19 -0500

    add initial Python examples for loops and conditionals

---
+++ sesion-1/ejemplos/ciclo.py
@@ -0,0 +1,17 @@
+# Ciclo For, recorre los elementos o repite una cantidad de veces
+
+# ciclo de rango maximo 5
+for i in range(5):
+    print(i)
+
+# ciclo de rango de lista
+for fruta in ["manzanza", "fresa", "platano"]:
+    print(fruta)
+
+# ciclo de rango de inicio y fin
+for i in range(2, 5):
+    print(i)
+
+# ciclo de rango negativo y positivo
+for i in range(-2, 2):
+    print(i)
---
+++ sesion-1/ejemplos/condicionales.py
@@ -0,0 +1,19 @@
+# Condicionales
+# if, elif, else
+
+# Ejemplo if - else
+edad = 20
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+# Ejemplo if - elif - else
+nota = 15
+
+if nota >= 18:
+    print("Excelente")
+elif nota >= 13:
+    print("Aprobado")
+else:
+    print("Desaprobado")
