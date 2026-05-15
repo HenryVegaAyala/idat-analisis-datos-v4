ff9b2daad8ed6271b8ab03f5bf78cf8794ffd512
Henry Vega <hp.vega@hotmail.com>
2026-05-14 21:04:55 -0500

    add .gitignore and initial Python examples for list operations

--- sesion-2/main.py
+++ sesion-2/main.py
@@ -6,15 +6,15 @@ venta_horas = [10, 20, 15, 20]
 
 # 3. Acceder al primero elemento de una lista (indice 0)
 primer_producto = vitrina[0]
-print(f"Primer producto: {primer_producto}" )
+print(f"Primer producto: {primer_producto}")
 
 # 4. Acceder al ultimo elemento de una lista (indice -1)
 ultimo_producto = vitrina[-1]
-print(f"Ultimo producto: {ultimo_producto}" )
+print(f"Ultimo producto: {ultimo_producto}")
 
 # 5. Contar cuantos elementos hay en una lista
 total_productos = len(vitrina)
-print(f"Total de productos: {total_productos}" )
+print(f"Total de productos: {total_productos}")
 
 # 6. Agregar nuevo producto a la lista
 vitrina.append("Donut")
@@ -23,3 +23,20 @@ print(f"Lista de productos agregados: {vitrina}")
 # 7. Cambiar o actualizar un producto de la lista
 vitrina[2] = "Aceite de oliva"
 print(f"Lista de productos actualizados: {vitrina}")
+
+# 8. Eliminar producto de la lista por item
+vitrina.remove("Pan")
+print(f"Lista de productos actualizados: {vitrina}")
+
+# 9. Eliminar producto de la lista por indice
+del vitrina[0]
+print(f"Lista de productos actualizados: {vitrina}")
+
+# 10. Como saber que item he eliminado usando POP
+producto_eliminado = vitrina.pop(1)
+print(f"Vitrina despues de usar pop: {vitrina}")
+print(f"Producto eliminado usando pop: {producto_eliminado}")
+
+# 11. Verificar si existe un producto en la lista
+existe_donut = "Donut" in vitrina
+print(f"Existe donut: {existe_donut}")
