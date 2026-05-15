e15507a629b7c954bb5b6320db6f720ab06d1f93
Henry Vega <hp.vega@hotmail.com>
2026-05-14 22:23:11 -0500

    add product verification and cleaning logic in main.py

--- sesion-2/conversion_moneda.py
+++ sesion-2/conversion_moneda.py
@@ -1,9 +1,15 @@
-def convertir_soles(monto_dolares, tipo_de_cambio = 3.80):
-    resultado = monto_dolares * tipo_de_cambio
+def convertir_moneda(monto, tipo_de_cambio = 3.80, conversion_soles = True):
+
+    if conversion_soles:
+        resultado = monto / tipo_de_cambio # Si conversion_soles es verdadero el monto a ingresar es dolares y lo convertire a soles
+    else:
+        resultado = monto * tipo_de_cambio # Si conversion_soles es falso el monto a ingresar es soles y lo convertire a dolares
+
+
     return resultado
 
-monto_soles = convertir_soles(100)
-print(f"Monto en soles: {monto_soles}")
+monto_total_soles = convertir_moneda(100, 3.85, False)
+print(f"Monto en soles: {monto_total_soles}")
 
-monto_soles = convertir_soles(200, 4)
-print(f"Monto en soles: {monto_soles}")
+monto_total_dolares = convertir_moneda(200, 3.60, True)
+print(f"Monto en dolares: {monto_total_dolares}")
